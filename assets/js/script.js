e531c6ef4fc49f59df42a83186086f8d6a9b4043
Doug Peterson <douglaspeterson6@gmail.com>
2021-09-05 16:14:57 -0500

    quiz missing timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,145 @@
+//container for quiz
 const containerEl = document.querySelector("#container");
-console.log(containerEl);
+
+//reusable answer buttons for each question
+const answerButtons =[
+    {btn: document.createElement("button")},
+    {btn: document.createElement("button")},
+    {btn: document.createElement("button")},
+    {btn: document.createElement("button")},
+];
+
+//High Score Button
+const highScoreButton = document.querySelector("#highScore");
+highScoreButton.addEventListener("click", function(){
+    clear();
+   let highScore = document.createElement("p");
+    containerEl.appendChild(highScore);
+    highScore.textContent = score;
+    container.appendChild(homeButton);
+});
+
+//timer elements
+let timerEl = document.querySelector("#timer");
+
+//home button
+const homeButton = document.createElement("button");
+homeButton.textContent = "Main Screen";
+homeButton.addEventListener("click", function(){
+    startScreen();
+});
+
+//array of questions
+const questions = [
+    {
+        q: "What is the file extension for JavaScript?", 
+        a: [".HTML",".js",".xml",".css"],
+        c: ".js"
+    },
+    {
+        q: "What does HTML stand for?", 
+        a: ["Hyper text markup lasagna","Hippos talk my language","Hyper tool markup language","Hyper text markup language"],
+        c: "Hyper text markup language"
+    },
+    {
+        q: "What symbol do you use to end a line in JavaScript?", 
+        a: [".","#",";","!"],
+        c: ";"
+    },
+    {
+        q: "What symbol is at the end of the function?", 
+        a: ["}",")","!","]"],
+        c: "}"
+    },
+    {
+        q: "Which is not a proper way to create variable in JS?", 
+        a: ["const","var","let","setVar"],
+        c: "setVar"
+    },
+];
+
+let score = 10;
+let questionNumber = 0;
+let highScore = localStorage.getItem("High Score");
 
 
-
-const questions = {
-    q: "What color is the sky?"
-}
+//start screen elements
+const h2El = document.createElement("h2");
+const startButton = document.createElement("button");
+const pEl = document.createElement("p");
 
 //start screen that will display when the user is ready to take quiz
-function startScreen() {
-    //start screen elements
-    const h2El = document.createElement("h2");
-    const pEl = document.createElement("p");
-    const btnEl = document.createElement("button");
-
+function startScreen() {    
+    clear();
     containerEl.appendChild(h2El);
     containerEl.appendChild(pEl);
-    containerEl.appendChild(btnEl);
+    containerEl.appendChild(startButton);
     h2El.textContent = "Coding Quiz Challenge";
     pEl.textContent = "Test your coding knowledge with these questions!";
-    btnEl.textContent = "Start Quiz";
-    btnEl.style = "width: 30%; margin-top: 40px;";
+    startButton.textContent = "Start Quiz";
 
+    startButton.addEventListener("click", function(){
+        checkAnswer();
+    });
+}
+
+//clears container allowing more objects to be appended to
+function clear() {
+    containerEl.innerHTML = "";
+    containerEl.appendChild(h2El);
+}
+
+//fills answerButtons depending on what question number the user is on
+function nextQuestion(num){
+    clear();
+
+    if(num < questions.length){
+        console.log(num);
+        for(let i = 0; i < answerButtons.length; i++) {
+            containerEl.appendChild(answerButtons[i].btn);
+            answerButtons[i].btn.textContent = questions[num].a[i];
+        }
+        h2El.textContent = questions[num].q;
+    }
+    else {
+        questionNumber = 0;
+        score = 0;
+        endQuiz();
+    }
+}
+
+//checks answers 
+function checkAnswer(){
+    nextQuestion(questionNumber);
+    
+    for (let i = 0; i < answerButtons.length; i++){
+        answerButtons[i].btn.addEventListener("click",function(){
+            if (answerButtons[i].btn.textContent === questions[questionNumber].c){
+                console.log(answerButtons[i].btn.textContent);
+            }
+            else {
+                console.log(answerButtons[i].btn.textContent);   
+                score--;    
+            }
+            nextQuestion(questionNumber);
+            questionNumber++;
+
+            checkAnswer();
+        });
+    }
+}
+
+
+
+//ends quiz and displays high score 
+function endQuiz() {
+    clear();
+    h2El.textContent = "Congrats you got " + score;
+    if (score > localStorage.getItem("High Screen")){
+        localStorage.setItem("High Score", score);
+    }
+    containerEl.appendChild(homeButton);
+    
 }
 
 startScreen();
